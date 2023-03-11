ae86fea93d95067a261d6ec8301d549ddc721e57
Thomas Van <thomascvan11@gmail.com>
2023-03-10 16:41:49 -0800

    added basic map functionality

--- client/src/Components/Map/Map.jsx
+++ client/src/Components/Map/Map.jsx
@@ -1,47 +1,46 @@
-import React, { Component, useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
 
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 export function Map() {
-  function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 10,
-      center: { lat: 33.80421371292495, lng: -117.99600029783456 },
-    });
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyAg2NCnQUGMiMs28H0Uhorp2ELrZBvxvk8"  // publicly available API key
+  })
 
-    const locations = [
-      {
-        lat: 33.80421371292495,
-        lng: -117.99600029783456,
-        icon: "https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485565/pngwing.com_4_hssthb.png",
-      },
-      {
-        lat: 33.894377805345435,
-        lng: -117.87114238988885,
-        icon: "https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485700/pngwing.com_3_ja6dw9.png",
-      },
-      {
-        lat: 34.06103499718833,
-        lng: -117.18623527454041,
-        icon: "https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485565/pngwing.com_1_ka3o33.png",
-      },
-    ];
+  const [map, setMap] = React.useState(null)
 
-    for (let i = 0; i < locations.length; i++) {
-      const marker = new google.maps.Marker({
-        position: { lat: locations[i].lat, lng: locations[i].lng },
-        map,
-        icon: locations[i].icon,
-      });
-    }
-  }
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
 
-  window.initMap = initMap;
+    setMap(map)
+  }, [])
 
-  return (
-    <div id="map">
-      <h1>Map</h1>
-    </div>
-  )
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
 }
