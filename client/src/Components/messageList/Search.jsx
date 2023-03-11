339cf35465c8d69f78bed5e2dcb88e92409409af
Tony Vo <vo.tony12@gmail.com>
2023-03-11 14:27:53 -0700

    created a searchTile component and renders after search

--- client/src/Components/messageList/Search.jsx
+++ client/src/Components/messageList/Search.jsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import {SearchTile} from './SearchTile.jsx';
 
 export function Search () {
 
   const [search, setSearch] = useState('');
+  const [tileStatus, setTileStatus] = useState(false)
 
   const handleSearch = (event) => {
     setSearch(event.target.value);
@@ -12,7 +14,9 @@ export function Search () {
   const submitSearch = (event) => {
     console.log('this is the search: ',search);
     event.preventDefault();
-    axios.post('/searchFriend', {searchQuery: search});
+    // axios.post('/searchFriend', {searchQuery: search});
+    // after sucessful search, change tileStatus to true
+    setTileStatus(true);
   }
 
   return (
@@ -21,6 +25,7 @@ export function Search () {
         <input type="text" name="searchQuery" onChange={handleSearch}/>
         <input type="submit" name="search" onClick={submitSearch}/>
       </form>
+      {tileStatus ? <SearchTile /> : null}
     </div>
   )
 }
