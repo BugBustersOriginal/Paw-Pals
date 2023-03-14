b95b8c852247b2ad7ec53cace8c5fa99094dcd6e
Andymamooo <andyderkaema@gmail.com>
2023-03-14 10:08:59 -0700

    add .post mongodb middleware to delete images from messages and conversation

--- db/index.js
+++ db/index.js
@@ -10,9 +10,26 @@ let messagesSchema = new mongoose.Schema({
   content: String,
   createdAt: {type: Date, default: Date.now},
   openedAt: Date,// field to record when a photo was opened
+  // set index so mongo can delete images faster.
   expiresAt: {type: Date, index : {expireAfterSeconds:0}},
-  type: {type: String, enum:['text', 'image'], required: true}
+  type: {type: String, enum:['text', 'image'], required: true},
+  conversationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'Conversation',
+    required: true
+  }
 })
+
+// mongoose middleware that will be triggered after a document has been deleted from the 'messagesSchema' collection
+messagesSchema.post('findOneAndDelete', async function (doc) {
+  // doc contains the deleted message document
+  const conversationId = doc.conversationId;
+  //update the conversation document after
+  await Conversation.findByIdAndUpdate(conversationId, {
+    //pull operator removes from an existing array all instances of a value or values that match a specified conditin
+    $pull: { messages: doc._id }
+  });
+});
 
 let conversationsSchema = new mongoose.Schema({
  participiants : [String],
@@ -20,8 +37,6 @@ let conversationsSchema = new mongoose.Schema({
 })
 let Message = mongoose.model('Messages', messagesSchema);
 let Conversation = mongoose.model('Conversations', conversationsSchema);
-
-
 
 
 module.exports = { Message, Conversation}
