0b7edb27c75a9931d28cb5a51beaa2702176a106
Andymamooo <andyderkaema@gmail.com>
2023-03-14 12:11:25 -0700

    remove time to live feature and implemented viewed which a flag to keep track if a image has been viewed. also added more routes

--- db/index.js
+++ db/index.js
@@ -11,7 +11,8 @@ let messagesSchema = new mongoose.Schema({
   createdAt: {type: Date, default: Date.now},
   openedAt: Date,// field to record when a photo was opened
   // set index so mongo can delete images faster.
-  expiresAt: {type: Date, index : {expireAfterSeconds:0}},
+  //expiresAt: {type: Date, index : {expireAfterSeconds:0}},
+  viewed:{type:Boolean, default: false},
   type: {type: String, enum:['text', 'image'], required: true},
   conversationId: {
     type: mongoose.Schema.Types.ObjectId,
@@ -21,15 +22,15 @@ let messagesSchema = new mongoose.Schema({
 })
 
 // mongoose middleware that will be triggered after a document has been deleted from the 'messagesSchema' collection
-messagesSchema.post('findOneAndDelete', async function (doc) {
-  // doc contains the deleted message document
-  const conversationId = doc.conversationId;
-  //update the conversation document after
-  await Conversation.findByIdAndUpdate(conversationId, {
-    //pull operator removes from an existing array all instances of a value or values that match a specified conditin
-    $pull: { messages: doc._id }
-  });
-});
+// messagesSchema.post('findOneAndDelete', async function (doc) {
+//   // doc contains the deleted message document
+//   const conversationId = doc.conversationId;
+//   //update the conversation document after
+//   await Conversation.findByIdAndUpdate(conversationId, {
+//     //pull operator removes from an existing array all instances of a value or values that match a specified conditin
+//     $pull: { messages: doc._id }
+//   });
+// });
 
 let conversationsSchema = new mongoose.Schema({
  participiants : [String],
--- index.js
+++ index.js
@@ -4,17 +4,78 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
 const path = require('path');
 const PORT = process.env.PORT;
+const cors = require('cors');
 const {Conversation, Message} = require('./db/index.js');
+const {setPhotoExpiration,sendMessage}=('./db/helperFunctions.js')
+
 
 app.use(express.json());
-
-
+const io = new Server(server, {
+  cors :{
+    origin : 'http://localhost:1234',
+    methods:['GET','POST','PUT']
+  }
+})
 
 app.get('/', (req, res) => {
   res.send("connected");
+});
+
+app.post("/openedImage/:id", async (req, res) => {
+  const imageId = req.params.id;
+  try {
+    const message = await Message.findByIdAndUpdate(imageId, {
+      viewed: true,
+      viewTime: new Date()
+    });
+    // Update corresponding conversation
+    const conversationId = message.conversationId;
+    await Conversation.findByIdAndUpdate(conversationId, {
+      $set: {
+        "messages.$[elem].viewed": true,
+        "messages.$[elem].viewTime": new Date()
+      }
+    }, {
+      arrayFilters: [{ "elem._id": imageId }]
+    });
+    res.status(200).send({ message });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'An error occurred while updating an viewed Image' });
+  }
+});
+
+
+// use to get the whole conversation when a chat is open
+app.get('/conversation/:id', async (req, res) => {
+  try {
+    const conversationId = req.params.id;
+    const conversation = await Conversation.findById(conversationId) // Populate the 'participants' field with only the 'name' attribute
+
+    if (!conversation) {
+      return res.status(404).json({ error: 'Conversation not found' });
+    }
+    return res.json(conversation);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// messageslist will use this route to get all conversations?
+app.get("/conversations/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const conversations = await Conversation.find({
+      participants: { $elemMatch: { $eq: userId } }
+    });
+    res.json(conversations);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 });
 
 io.on('connection', (socket) => {
