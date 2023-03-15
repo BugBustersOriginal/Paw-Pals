c9527ad7e654ba2dd23c0ad65fa3cba8492a68c8
Tony Vo <vo.tony12@gmail.com>
2023-03-15 17:14:44 -0600

    set up Jest and installed React Testing Library

---
+++ tests/searchFriend.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render, screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/react'
+
+import App from '../client/src/App.jsx';
+import Search from '../client/src/Components/messageList/Search/Search.jsx'
+
+describe('Test Suite', () => {
+  it('My Test Case', () => {
+    expect(true).toEqual(true);
+  });
+});
+
