fe6e4ec77275120d85db584609713c6f3080ab03
Thomas Van <thomascvan11@gmail.com>
2023-03-16 16:08:15 -0700

    added markers with custom images

--- client/src/Components/Map/Map.jsx
+++ client/src/Components/Map/Map.jsx
@@ -21,6 +21,25 @@ function MapView() {
     zoom={10}
     center={{lat: 44, lng: -80}}
     mapContainerClassName="map-container"
-    ></GoogleMap>
+    >
+            <Marker
+        position={{lat: 44, lng: -80}}
+        icon={{
+          url: 'https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485565/pngwing.com_1_ka3o33.png'
+        }}
+      />
+      <Marker
+        position={{lat: 44, lng: -79.9}}
+        icon={{
+          url: 'https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485565/pngwing.com_4_hssthb.png'
+        }}
+      />
+      <Marker
+        position={{lat: 44, lng: -79.8}}
+        icon={{
+          url: 'https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485700/pngwing.com_3_ja6dw9.png'
+        }}
+      />
+    </GoogleMap>
   );
 }
