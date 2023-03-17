3aeddf890a81e106f5dd25f7b863ad735760db68
Thomas Van <thomascvan11@gmail.com>
2023-03-16 18:55:35 -0700

    added props, screen center state, and mapping to populate map data based off of dummy data

--- client/src/Components/Map/Map.jsx
+++ client/src/Components/Map/Map.jsx
@@ -3,45 +3,66 @@ import { useMemo } from 'react'
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 export function Map() {
-  const [apiKey, setApiKey] = useState('');
+
+  let user = {
+    name: 'Thomas',
+    location: {lat: 44, lng: -80},
+    profileImage: 'https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485565/pngwing.com_1_ka3o33.png'
+  }
+
+  let friends = [
+    {
+      name: 'Andy',
+      location: {lat: 44, lng: -79.5},
+      profileImage: 'https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485565/pngwing.com_4_hssthb.png'
+    },
+    {
+      name: 'Tony',
+      location: {lat: 44, lng: -79},
+      profileImage: 'https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485700/pngwing.com_3_ja6dw9.png'
+    }
+  ]
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyB1le7LEHbnuufJ03zPAF2Mh1xlmszRo4U" // restricted key
   });
 
   if (!isLoaded) return <div>Loading...</div>
+
   return (
-    <MapView />
+    <MapView user={user} friends={friends}/>
   )
 }
 
-function MapView() {
+function MapView({ user, friends }) {
+  const [screenCenter, setScreenCenter] = useState(user.location);
+
   return (
     <>
-    <GoogleMap
-    zoom={10}
-    center={{lat: 44, lng: -80}}
-    mapContainerClassName="map-container"
-    >
-            <Marker
-        position={{lat: 44, lng: -80}}
-        icon={{
-          url: 'https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485565/pngwing.com_1_ka3o33.png'
-        }}
+      <GoogleMap
+      zoom={10}
+      center={screenCenter}
+      mapContainerClassName="map-container"
+      >
+
+        {/* generate user's location */}
+        <Marker
+          position={user.location}
+          icon={{
+            url: user.profileImage
+          }}
         />
-      <Marker
-        position={{lat: 44, lng: -79.9}}
-        icon={{
-          url: 'https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485565/pngwing.com_4_hssthb.png'
-        }}
+
+        {/* generate friends' location */}
+        {friends.map((friend, idx) =>
+          <Marker key={idx}
+          position={friend.location}
+          icon={{
+            url: friend.profileImage
+          }}
         />
-      <Marker
-        position={{lat: 44, lng: -79.8}}
-        icon={{
-          url: 'https://res.cloudinary.com/ddu3bzkvr/image/upload/v1678485700/pngwing.com_3_ja6dw9.png'
-        }}
-        />
-    </GoogleMap>
+        )}
+      </GoogleMap>
     </>
   );
 }
