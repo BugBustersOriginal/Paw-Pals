7134ca45569df00fc76562bc58bf97677faa2cc8
MA Hereford <bluechocolate2019@gmail.com>
2023-03-21 12:13:01 -0700

    axios request to retrieve dummy data from frontend server

--- client/src/Components/MessageList/FriendTileList.jsx
+++ client/src/Components/MessageList/FriendTileList.jsx
@@ -4,26 +4,23 @@ import FriendTile from './FriendTile.jsx';
 import axios from 'axios'
 
 const FriendTileList = (props) => {
-  //will receive {profileInfo}, {mostRecentMessages} as props
+  //will receive userId from App.jsx
   console.log(props, 'line 9 FriendTileList')
 
   const [chats, setChats] = useState([])
 
+  //will use userId to retrieve chats from message server
   useEffect(() => {
     axios.post('/conversations/' + props.userId)
       .then((res) => {
+        console.log(res.data, 'line 16 FriendTileList')
         setChats(res.data)
       })
-
   }, [])
-
-
 
   return (
     <div>
       <Search userId={props.userId} />
-      FriendTileList
-
       {chats.map((chat) => <FriendTile chat={chat} userId={props.userId}/>)}
     </div>
   )
