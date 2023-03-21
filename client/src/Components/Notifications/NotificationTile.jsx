b2e5415d519d02b5f66b480398d2fc9d9e6a244a
Tony Vo <vo.tony12@gmail.com>
2023-03-21 16:57:24 -0600

    created buttons for friend request

--- client/src/Components/Notifications/NotificationTile.jsx
+++ client/src/Components/Notifications/NotificationTile.jsx
@@ -4,8 +4,12 @@ export function NotificationTile (props) {
   // console.log('friend tile: ', props.friend)
   return (
     <div className="notification-tile">
-      <img className="user-photo-thumbnail" src={props.thumbnailUrl}/>
-      <p>@{props.userId}</p>
+      <div>
+        <img className="user-photo-thumbnail" src={props.thumbnailUrl}/>
+        <p>@{props.userId}</p>
+      </div>
+      <p>Sent you a friend request!</p>
+      <button>Accept</button>
     </div>
   )
 }
