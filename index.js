1bfa0865423e2b4e70077acab4c6f0997badf727
Yaser Eisa <yasereisa@Yasers-MacBook-Air.local>
2023-03-21 22:17:51 -0400

    snap

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const path = require('path');
 const PORT = process.env.PORT;
 const cors = require('cors');
 const {Conversation, Message, FriendList} = require('./db/index.js');
-const {setPhotoExpiration,sendMessage}=('./db/helperFunctions.js')
+
 
 
 app.use(express.json());
 const io = new Server(server, {
   cors :{
-    origin : 'http://localhost:3033',
+    origin : 'http://localhost:1234',
     methods:['GET','POST','PUT']
   }
 })
@@ -46,6 +46,12 @@ app.post("/openedImage/:id", async (req, res) => {
     res.status(500).send({ error: 'An error occurred while updating an viewed Image' });
   }
 });
+
+
+
+
+
+
 
 
 // use to get the whole conversation when a chat is open
@@ -99,11 +105,24 @@ io.on('connection', (socket) => {
    socket.on('new-message', async (data) => {
     if(data !== ''){
       console.log("data", data)
+      try {
+        const message = await Message.create(data);
+        const conversation = await Conversation.findOneAndUpdate(
+          {_id:data.conversationId},
+          {$push: {messages:message}},
+          {new:true} // returns back the conversation after adding new message
+        )
+      // Broadcast message to all users in the conversation room
+      const roomNames = Object.keys(io.sockets.adapter.rooms).filter(roomId => !io.sockets.adapter.rooms[roomId].sockets[roomId]);
+        io.to(data.conversationId).emit('new-message', message);
+      } catch (err) {
+        console.error(`error while sending new-message ${err}`)
+      }
     // Save message to database
-    // const message = await Message.create(data);
+    const message = await Message.create(data);
 
     // // Broadcast message to all users in the conversation
-    // socket.to(data.conversationId).emit('new-message', message);
+    socket.to(data.conversationId).emit('new-message', message);
     }
 
 
