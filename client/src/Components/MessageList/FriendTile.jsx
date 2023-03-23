3d47b4672e43fe7e672685f3c92bf689a359f8cd
MA Hereford <bluechocolate2019@gmail.com>
2023-03-22 20:14:17 -0700

    fixed undefined props

--- client/src/Components/MessageList/FriendTile.jsx
+++ client/src/Components/MessageList/FriendTile.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import { Route, Routes, useNavigate, useLocation, withRouter, redirect } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const FriendTile = ( {chat, userId} ) => {
-  console.log(chat, 'line 4 chat from FriendTile')
-  console.log(userId, 'line 5 userId from FriendTile')
 
   const navigate = useNavigate();
-  const location = useLocation();
 
   const handleClick = () => {
-    //goes to conversation window
     navigate('/messagewindow')
-
     //send friendUserName to message window
-    //navigate to message window
-
   }
 
   const otherParticipant = chat.participants.find((e) => {
@@ -26,7 +19,7 @@ const FriendTile = ( {chat, userId} ) => {
 
   return (
     <div className="friend-tile-container">
-        <div className="friend-tile-username">{otherParticipant.username}</div>
+        <div className="friend-tile-username" onClick={handleClick}>{otherParticipant.username}</div>
         <div>{otherParticipant.profileIcon}</div>
         <div onClick={handleClick}>{latestMessage.content}</div>
         <div className="friend-time-timestamp">{latestMessage.createdAt}</div>
--- client/src/Components/MessageList/FriendTileList.jsx
+++ client/src/Components/MessageList/FriendTileList.jsx
@@ -3,17 +3,17 @@ import { Search } from './Search/Search.jsx';
 import FriendTile from './FriendTile.jsx';
 import axios from 'axios'
 
-const FriendTileList = ( props, {userId} ) => {
+const FriendTileList = ( props ) => {
   //will receive userId from App.jsx
-  // console.log(props, 'line 8 FriendTileList')
-  console.log(userId, 'line 9 userID from FriendTileList')
+  console.log(props.userId, 'line 8 props FriendTileList')
+  // console.log(userId, 'line 9 userID from FriendTileList')//undefined
   // const userName = userId
 
   const [chats, setChats] = useState([])
 
   //will use userId to retrieve chats from message server
   useEffect(() => {
-    axios.post('/conversations/' + userId)
+    axios.post('/conversations/' + props.userId)
       .then((res) => {
         console.log(res.data, 'line 16 FriendTileList')
         setChats(res.data)
@@ -22,8 +22,8 @@ const FriendTileList = ( props, {userId} ) => {
 
   return (
     <div>
-      <Search userId={userId} userFriends={props.userFriends} userInfo={props.userInfo} userId={props.userId} pendingRequests={props.pendingRequests} />
-      {chats.map((chat) => <FriendTile chat={chat} userId={userId}/>)}
+      <Search userFriends={props.userFriends} userInfo={props.userInfo} userId={props.userId} pendingRequests={props.pendingRequests} />
+      {chats.map((chat) => <FriendTile chat={chat} userId={props.userId}/>)}
     </div>
   )
 }
