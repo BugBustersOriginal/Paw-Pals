f70f12ac884d56504eab52d9f9200329856cb8f3
debrazhang <debrazhang09@gmail.com>
2023-03-22 20:54:35 -0700

    add gmail account as required in register

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(session({
   secret: 'pawpal',
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge:  5* 60 * 1000}
+  cookie: { maxAge:  5* 60 * 1000} //5mins valid
   })
 );
 
