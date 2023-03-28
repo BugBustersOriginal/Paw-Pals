1a3cd4b3693b2cee63ac27c0374eac0c164f652f
MA Hereford <bluechocolate2019@gmail.com>
2023-03-28 10:58:50 -0700

    added feature to navigate to update UserOne and UserTwo when navigating to message window

--- client/src/Components/MessageList/FriendTile.jsx
+++ client/src/Components/MessageList/FriendTile.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const FriendTile = ( {friend} ) => {
-  console.log(friend, 'line 5 friend FriendTile')
+const FriendTile = ( {conversation, userId} ) => {
+  console.log(conversation.friend, 'line 5 conversation FriendTile')
+  console.log(userId, 'line 6, FriendTile')
+  console.log(conversation.messages, 'line 7')
 
   const navigate = useNavigate();
 
   const handleClick = (e) => {
-    navigate('/messagewindow', {state: {}})
-    // navigate('/messagewindow')
+    let userOne = userId;
+    let userTwo = conversation.friend;
+    let participants = [userOne, userTwo]
+    navigate('/messagewindow', {state: {
+      users: participants,
+      currentUser: userOne,
+      userTwo: userTwo
+    }})
   }
 
   return (
     <div className="friend-tile-container">
       <img className="user-photo-thumbnail" src="https://pbs.twimg.com/profile_images/874661809139073025/X8yzIhNy_400x400.jpg/"/>
-      <div className="friend-tile-username" onClick={handleClick}>{friend.friend}</div>
-      <div>{friend.messages? friend.messages[friend.messages.length-1].content : 'no messages yet'}</div>
-      <div>{friend.messages? friend.messages[friend.messages.length-1].createdAt : ''}</div>
+      <div className="friend-tile-username" onClick={handleClick}>{conversation.friend}</div>
+      <div>{conversation.messages? conversation.messages[conversation.messages.length-1].content : 'no messages yet'}</div>
+      <div>{conversation.messages? conversation.messages[conversation.messages.length-1].createdAt : ''}</div>
     </div>
   )
+  // return (
+  //   <div className="friend-tile-container">
+  //     <img className="user-photo-thumbnail" src="https://pbs.twimg.com/profile_images/874661809139073025/X8yzIhNy_400x400.jpg/"/>
+  //     <div className="friend-tile-username" onClick={handleClick}>{friend.friend}</div>
+  //     <div>{friend.messages? friend.messages[friend.messages.length-1].content : 'no messages yet'}</div>
+  //     <div>{friend.messages? friend.messages[friend.messages.length-1].createdAt : ''}</div>
+  //   </div>
+  // )
 }
 
 export default FriendTile;
