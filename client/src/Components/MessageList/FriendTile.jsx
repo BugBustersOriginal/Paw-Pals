ea159d185a4390554788133d6df3b7bac9bcb477
MA Hereford <bluechocolate2019@gmail.com>
2023-03-28 21:09:43 -0700

    formated timestamp

--- client/src/Components/MessageList/FriendTile.jsx
+++ client/src/Components/MessageList/FriendTile.jsx
@@ -11,6 +11,16 @@ const FriendTile = ( {conversation, userId, userInfo} ) => {
     conversation.messages = []
   }
   console.log(conversation.messages, 'line 9 Friendtile')
+  console.log(conversation.messages[conversation.messages.length-1].content, 'line 14')
+  console.log(conversation.messages[conversation.messages.length-1].createdAt, 'line 15')
+
+  let formattedDate = new Date(conversation.messages[conversation.messages.length-1].createdAt)
+  console.log(formattedDate, 'line 18')
+
+  let formattedTime = formattedDate.toLocaleString('en-US', {hour12: true, hour: 'numeric', minute: '2-digit'});
+  console.log(formattedTime, 'line 21')
+
+
 
   const navigate = useNavigate();
 
@@ -25,15 +35,35 @@ const FriendTile = ( {conversation, userId, userInfo} ) => {
     }})
   }
 
+  // function getLastMessageContent(conversation) {
+  //   const lastMessage = conversation.messages[conversation.messages.length-1];
+
+  //   if (lastMessage && lastmessage.type === 'image') {
+  //     return 'I just sent you a photo!';
+  //   } else {
+  //     return lastMessage ? lastMessage.content : 'no messages yet';
+  //   }
+  // }
+
   return (
     <div className="friend-tile-container" onClick={handleClick}>
       <img className="user-photo-thumbnail" src={conversation.friend.thumbnailUrl}/>
       <div>
         <div className="name-timestamp space-between">
           <div>{conversation.friend.userId}</div>
-          <div>{conversation.messages.length > 0? conversation.messages[conversation.messages.length-1].createdAt : ''}</div>
+          <div>{conversation.messages.length > 0? formattedTime : ''}</div>
         </div>
-        <div>{conversation.messages.length > 0? conversation.messages[conversation.messages.length-1].content : 'no messages yet'}</div>
+        {/* <div>{getLastMessageContent(conversation)}</div> */}
+        <div>{conversation.messages.length > 0 ? (
+          conversation.messages[conversation.messages.length-1].type === 'image' ? (
+            'I just sent you a photo!'
+          ) : (
+            conversation.messages[conversation.messages.length-1].content
+          )
+          ) : (
+            'no messages yet'
+          )}
+          </div>
     </div>
     </div>
   )
