08a6136796207a5bacb952defe1099fbd1a28639
Tony Vo <vo.tony12@gmail.com>
2023-03-30 15:07:33 -0600

    fixed jest testing for browser router

--- tests/searchFriend.spec.js
+++ tests/searchFriend.spec.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, fireEvent, screen, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/react'
+import setImmediate from 'setImmediate';
+// global.setImmediate = setImmediate;
 
 import {App} from '../client/src/App.jsx';
+
 import {Search} from '../client/src/Components/messageList/Search/Search.jsx';
 import {SearchTile} from '../client/src/Components/messageList/Search/SearchTile.jsx';
+import { MemoryRouter, Route, Routes, useNavigate, useLocation, withRouter, BrowserRouter } from 'react-router-dom';
 
 const userInfo = {
   thumbnailUrl: 'https://hs.sbcounty.gov/cn/Photo%20Gallery/_w/Sample%20Picture%20-%20Koala_jpg.jpg',
@@ -15,13 +19,14 @@ const userInfo = {
 
 describe('Search Component Test Suite', () => {
   it('Search Component Renders', () => {
-    render(<App />)
-    const searchBar = screen.getByTestId('search-component');
-    expect(searchBar).toBeInTheDocument();
+    render(      <MemoryRouter>
+      <Search />
+    </MemoryRouter>)
+    expect(screen.getByTestId('search-component')).toBeInTheDocument();
   });
 
   it('Input text into Search Bar', () => {
-    render(<App />)
+    render(<MemoryRouter><Search /></MemoryRouter>)
     const input = screen.getByTestId("search-input");
     fireEvent.change(input, { target: { value: 'tivo' } });
     const query = screen.getByDisplayValue('tivo');
