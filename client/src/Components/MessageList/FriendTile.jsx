b6317e591051b136848f15bc281372a64857dab4
Andymamooo <andyderkaema@gmail.com>
2023-03-30 19:04:33 -0700

    adding mary

--- client/src/Components/MessageList/FriendTile.jsx
+++ client/src/Components/MessageList/FriendTile.jsx
@@ -1,41 +1,40 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Route, Routes, useNavigate, useLocation, withRouter, redirect } from 'react-router-dom';
 
-const FriendTile = ( {conversation, userId, userInfo} ) => {
-  // console.log(conversation.friend, 'line 5 conversation FriendTile')
-  // console.log(userId, 'line 6, FriendTile')
-  // console.log(conversation.messages, 'line 7')
-  // console.log(userInfo.thumbnailUrl, 'line 8')
-
-  if (conversation.messages === undefined) {
-    conversation.messages = []
-  }
-  console.log(conversation.messages, 'line 9 Friendtile')
+const FriendTile = ( {chat, userId} ) => {
+  console.log(chat, 'line 4 chat from FriendTile')
+  console.log(userId, 'line 5 userId from FriendTile')
 
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleClick = (e) => {
     let userOne = userId;
     let userTwo = conversation.friend.userId;
     let participants = [userOne, userTwo]
-    navigate('/messagewindow', {state: {
+    navigate(‘/messagewindow’, {state: {
       users: participants,
       currentUser: userOne,
-      userTwo: userTwo
+      userTwo: userTwo,
+      userTwoIcon: conversation.friend.thumbnailUrl
     }})
   }
 
+  const otherParticipant = chat.participants.find((e) => {
+    return e.userId !== userId;
+  })
+
+  let last = chat.messages.length-1
+  const latestMessage = chat.messages[last]
+
   return (
-    <div className="friend-tile-container" onClick={handleClick}>
-      <img className="user-photo-thumbnail" src={conversation.friend.thumbnailUrl}/>
-      <div>
-        <div className="name-timestamp space-between">
-          <div>{conversation.friend.userId}</div>
-          <div>{conversation.messages.length > 0? conversation.messages[conversation.messages.length-1].createdAt : ''}</div>
-        </div>
-        <div>{conversation.messages.length > 0? conversation.messages[conversation.messages.length-1].content : 'no messages yet'}</div>
+    <div className="friend-tile-container">
+        <div className="friend-tile-username">{otherParticipant.username}</div>
+        <div>{otherParticipant.profileIcon}</div>
+        <div onClick={handleClick}>{latestMessage.content}</div>
+        <div className="friend-time-timestamp">{latestMessage.createdAt}</div>
     </div>
-    </div>
+
   )
 }
 
