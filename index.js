4555bac2454e48f40b5affda43f938b496294d88
Yaser Eisa <yasereisa@Yasers-MacBook-Air.local>
2023-03-31 17:21:45 -0400

    added

--- index.js
+++ index.js
@@ -41,7 +41,9 @@ function getRoomsByUser(id){
 app.use(express.json());
 const io = new Server(server, {
   cors :{
+
     origin : [process.env.CORS_ORIGIN1, process.env.CORS_ORIGIN2],
+
     methods:['GET','POST','PUT']
   }
 })
