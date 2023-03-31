b27616a548ec7e309d056bd8938555f92ca0235b
Yaser Eisa <yasereisa@Yasers-MacBook-Air.local>
2023-03-31 17:40:15 -0400

    fixed axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import MessageWindow from './Components/MessageWindow/MessageWindow.jsx'
 import { Route, Routes, useNavigate, useLocation, withRouter } from 'react-router-dom';
 import { createBrowserHistory } from "history";
